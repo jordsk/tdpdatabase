c0795ae27aa2bebdc564fb2c3edad6e8740e0bf8
jordsk <jordan_806@hotmail.com>
2022-09-23 10:10:18 +0100

    trial1

--- Answers for World.sql
+++ Answers for World.sql
@@ -5,13 +5,11 @@ SELECT * FROM city;
 SELECT * FROM country;
 SELECT * FROM countrylanguage;
 
-SELECT COUNT(Name) FROM city WHERE country.code="Chn
+SELECT Name, SurfaceArea FROM country WHERE SurfaceArea ORDER BY SurfaceArea DESC limit 10;
 
-SELECT Name, Population FROM country WHERE (Population is NOT NULL 
-
-#SELECT DISTINCT country.Region, countrylanguage.language FROM country JOIN countrylanguage ON country.code=countrylanguage.CountryCode WHERE country.region="Southeast Asia";
+#SELECT DISTINCT country.Region, countrylanguage.language FROM country JOIN countrylanguage ON country.code=countrylanguage.CountryCode WHERE country.region=;
 #SELECT name, LifeExpectancy FROM country ORDER BY LifeExpectancy DESC limit 1;
-#SELECT Country.Name, city.Name FROM country JOIN city ON country.code=city.CountryCode WHERE city.id=(SELECT Capital FROM country WHERE name="Spain");
+#SELECT Country.Name, city.Name FROM country JOIN city ON country.code=city.CountryCode WHERE city.id=(SELECT Capital FROM country WHERE name="");
 
 
 #Brunei Indonesia Cambodia Laos Myanmar Malaysia Philippines Singapore Thailand East Timor Vietnam
@@ -25,3 +23,6 @@ SELECT Name, Population FROM country WHERE (Population is NOT NULL
 #56
 #SELECT city.Name FROM city WHERE city.Name LIKE "F%" LIMIT 25;
 #
+#SELECT Name, Population FROM country WHERE (Population is NOT NULL AND Population>0) ORDER BY Population ASC limit 1;
+#SELECT COUNT(Name) FROM country;
+#SELECT Name, SurfaceArea FROM country WHERE SurfaceArea ORDER BY SurfaceArea DESC limit 10;
